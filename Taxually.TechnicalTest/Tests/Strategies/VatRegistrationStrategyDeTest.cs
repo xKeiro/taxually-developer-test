7ebfc48420b8615b49eb7993f5de329b93b33f34
xKeiro <nkevin@outlook.hu>
2023-12-08 13:22:00 +0100

    Add test for VatRegistrationStrategyDe

---
+++ Taxually.TechnicalTest/Tests/Strategies/VatRegistrationStrategyDeTest.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using Taxually.TechnicalTest.Interfaces.Services.Clients;
+using Taxually.TechnicalTest.Models.Vat;
+using Taxually.TechnicalTest.Strategies.VatRegistration;
+
+namespace Tests.Strategies;
+
+[TestFixture]
+internal class VatRegistrationStrategyDeTest
+{
+    private VatRegistrationRequest _request = new()
+    {
+        CompanyId = "1",
+        Country = "DE",
+        CompanyName = "Test Company",
+    };
+
+    private Mock<ITaxuallyQueueClient> _mockQueueClient;
+    private VatRegistrationStrategyDe _strategy;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockQueueClient = new Mock<ITaxuallyQueueClient>();
+        _strategy = new VatRegistrationStrategyDe(_mockQueueClient.Object);
+    }
+
+    [Test]
+    public void Process_ShouldSerializeRequestToXmlAndEnqueueIt()
+    {
+        // Arrange
+        var expectedXml = @"<?xml version=""1.0"" encoding=""utf-16""?>
+<VatRegistrationRequest xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <CompanyName>Test Company</CompanyName>
+  <CompanyId>1</CompanyId>
+  <Country>DE</Country>
+</VatRegistrationRequest>";
+
+        // Act
+        _strategy.Process(_request);
+
+        // Assert
+        _mockQueueClient.Verify(m => m.EnqueueAsync("vat-registration-xml", expectedXml), Times.Once);
+    }
+
+}
