2e2357c57d53f09ff09f083595a64a22a86d4a05
xKeiro <nkevin@outlook.hu>
2023-12-08 13:22:45 +0100

    Add tests for CsvService

---
+++ Taxually.TechnicalTest/Tests/Services/CsvServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Taxually.TechnicalTest.Services.Utilities;
+
+namespace Tests.Services;
+
+[TestFixture]
+internal class CsvServiceTest
+{
+    private CsvService _csvService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _csvService = new CsvService();
+    }
+
+    [TestCase("Test Company", "1")]
+    [TestCase("0", "Zero")]
+    [TestCase("52", "53")]
+    [TestCase("Ékezetes", "Karakterek")]
+    public void getCompanyCsvString_ShouldReturnCsvString(string companyName, string companyId)
+    {
+        // Arrange
+        var expectedCsvString = $"CompanyName,CompanyId\r\n{companyName},{companyId}\r\n";
+        // Act
+        var csvString = _csvService.makeCompanyCsvString(companyName, companyId);
+        // Assert
+        Assert.That(csvString, Is.EqualTo(expectedCsvString));
+    }
+}
